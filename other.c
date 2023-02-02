122d3eb19defd11472fe699df4d9fa7f71c578a5
Leo T <leonard.t@bealy.io>
2023-02-02 14:41:31 +0100

    Added lots of comments with useful info, need to fix hex_dump

--- other.c
+++ other.c
@@ -12,7 +12,7 @@
 #define SMALL_SIZE 1400
 #define LARGE_SIZE 5400
 #define ALLOCATIONS 105
-#define TINY_ZONE (((ALLOCATIONS * (BLOCK_SIZE + TINY_SIZE) + ZONE_SIZE) / PAGE) + 1) * PAGE // this can most likely be done better
+#define TINY_ZONE (((ALLOCATIONS * (BLOCK_SIZE + TINY_SIZE) + ZONE_SIZE) / PAGE) + 1) * PAGE // this can most likely be done better // add TINY_ZONE_SIZE?
 #define SMALL_ZONE (((ALLOCATIONS * (BLOCK_SIZE + SMALL_SIZE) + ZONE_SIZE) / PAGE) + 1) * PAGE
 
 // ADD tree afer each malloc with size and pointer to go through each time and do the full block in half the time
@@ -34,7 +34,7 @@ typedef struct s_zone {
 typedef struct s_bucket {
 	t_zone *tiny;
 	t_zone *small;
-	t_zone *large;
+	t_zone *large; // this should be something else as large dont have a special "zone", they dont have block (as their blocks dont have next / prev / free) nor free space or type?
 } t_bucket;
 
 t_bucket	allocs;
@@ -64,6 +64,7 @@ void	*create_tiny(size_t size)
 		// rework this to be dynamic
 		// create mmap of zone
 		ret = mmap (NULL, TINY_ZONE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+		printf("start of tiny block %lu\n", ret);
 		allocs.tiny = ret;
 
 		// init and populate the zone in another func?
@@ -87,14 +88,16 @@ void	*create_tiny(size_t size)
 		allocs.tiny->block->next->next = ret + ZONE_SIZE + BLOCK_SIZE + BLOCK_SIZE + size;
 		allocs.tiny->block->next->size = TINY_ZONE - ZONE_SIZE - BLOCK_SIZE - BLOCK_SIZE - size;
 		allocs.tiny->block->next->free = 1; // 1 is free, 0 not free
-		printf("3 allocs.tiny %lu allocs.tiny->block %lu\n", allocs.tiny->block->next, allocs.tiny->block->next->next);
+		printf("3 allocs.tiny %lu allocs.tiny->block %lu\n", allocs.tiny->block + 1, allocs.tiny->block->next->next);
+
+		return (allocs.tiny->block + 1);
 		// printf("%lu\n", allocs.tiny->block->next->size);
 	}
-	// else if allocs.tiny isnt null and has free_space - BLOCK_SIZE > 0
+	// else if allocs.tiny isnt null and has free_space - (BLOCK_SIZE + size) > 0
 	else if (0)
 	{
 	}
-	// else allocs.tiny zone isnt null and theres no free_space so we need to create another mmap in allocs.tiny->next = mmap and bzero sizeof(t_zone)
+	// else allocs.tiny zone isnt null and theres no free_space so we need to create another mmap in allocs.tiny->next = mmap and bzero sizeof(TINY_ZONE)
 	// to init the pointers of the new zone to 0
 	else
 	{
@@ -104,23 +107,26 @@ void	*create_tiny(size_t size)
 
 void	*my_malloc(size_t size)
 {
+	void *ret = NULL;
 	if (size == 0)
 		return NULL;
 	size = go_next_block(size);
 	if (size < TINY_SIZE)
 	{ 
-		create_tiny(size); // check if tiny doesnt exist if it does check tehres space if there is use this
+		ret = create_tiny(size); // check if tiny doesnt exist if it does check tehres space if there is use this
 	}
-    return NULL;
+    return ret;
 }
 
 // add better format here
+// add itoa base here to transform to hex here? and write or printf %x
 void	hex_dump(void *s, size_t n)
 {
+	// reading byte by byte now needs to be displayed in hex, 0A for 10 ff for 255, etc
     size_t i = 0;
-    printf("Hex dump:\n");
-    while (i < n){
-        printf("%lu %x\n", i, ((char *)s)[i]);
+    printf("Hex dump of %lu:\n", s);
+    while (i < n) {
+        printf("%lu %d\n", i, ((char *)s)[i]);
         i++;
     }
     // printf("\n");
@@ -128,8 +134,15 @@ void	hex_dump(void *s, size_t n)
 
 int main()
 {
-	char *five = my_malloc(5);
-	// hex_dump(&allocs, sizeof(t_bucket));
+	size_t size = 5;
+	char *five = my_malloc(size);
+	for (size_t i = 0; i < size; ++i)
+	{
+		// printf("%lu\n", i);
+		five[i] = 'a';
+	}
+	hex_dump(five, size);
+	hex_dump(&allocs, 8);
 	printf("page size %d %lu\n", PAGE, TINY_ZONE / 240);
-	ft_bzero(&allocs, sizeof(t_bucket));
+	// ft_bzero(&allocs, sizeof(t_bucket));
 }
